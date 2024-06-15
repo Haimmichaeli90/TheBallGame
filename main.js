c510c6e995153f2560716d16ad5616c64c39ae20
Haimmichaeli90 <haimmichaeli95@gmail.com>
2024-06-15 13:53:34 +0300

    Limit the ball size diameter to 400px and reset it to 100px

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var ballSize = 100
 function onBallClick(){
    var ball = document.querySelector('.ball')
    ballSize = ballSize + 50
+   if(ballSize > 400){
+    ballSize = 100
+   }
    ball.innerText = ballSize
    ball.style.width = ballSize + 'px'
    ball.style.height = ballSize + 'px'
